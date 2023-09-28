6d655f7b557ab3af017a14c49a7d4131f897a1e9
zpc <zip_city@163.com>
2023-09-29 03:40:17 +0800

    change the position of Math.max and handle the end of array

--- LeetCode_Java/easy_lc485.java
+++ LeetCode_Java/easy_lc485.java
@@ -4,12 +4,13 @@ class Solution {
         for(int num : nums) {
             if(num == 1) {
                 range_len += 1;
-                max_len = Math.max(max_len, range_len);
             }
             else {
+                max_len = Math.max(max_len, range_len);
                 range_len = 0;
             }
         }
+        max_len = Math.max(max_len, range_len);
         return max_len;
     }
 }
