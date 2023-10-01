79f6cc98a888e04e8fd91216501013149bef2911
zpc <zip_city@163.com>
2023-10-02 04:19:04 +0800

    double pointer, convert to decimal numbers one by one

--- LeetCode_Java/medium_lc165.java
+++ LeetCode_Java/medium_lc165.java
@@ -1,19 +1,21 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+        int len1 = version1.length(), len2 = version2.length();
         int i = 0, j = 0;
-        while(i < v1.length || j < v2.length) {
+        while(i < len1 || j < len2) {
             int num1 = 0, num2 = 0;
-            if(i < v1.length) {
-                num1 = Integer.parseInt(v1[i]);
+            while(i < len1 && version1.charAt(i) != '.') {
+                num1 = version1.charAt(i) - '0' + num1 * 10;
                 i += 1;
             }
-            if(j < v2.length) {
-                num2 = Integer.parseInt(v2[j]);
+            i++;
+
+            while(j < len2 && version2.charAt(j) != '.') {
+                num2 = version2.charAt(j) - '0' + num2 * 10;
                 j += 1;
             }
-            
+            j++;
+
             if(num1 != num2) {
                 return num1 > num2 ? 1 : -1;
             }
