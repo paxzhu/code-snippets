f6541e2bd0ce21f3ee7c2228d3f6360672545036
zpc <zip_city@163.com>
2023-11-15 03:30:18 +0800

    write as an empty array

--- LeetCode_Java/easy_lc27.java
+++ LeetCode_Java/easy_lc27.java
@@ -1,28 +1,13 @@
-//Note: 快慢同向双指针
+// 读写指针，就当作在空数组写入
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if(nums.length == 0) {
-            return 0;
-        }
-        int slow = 0, fast = 1;
-        
-        while(fast < nums.length) {
-            if(nums[slow] == val) {
-                while(fast < nums.length && nums[fast] == val) {
-                    fast += 1;
-                }
-                if(fast == nums.length) {
-                    break;
-                }
-                nums[slow] = nums[fast];
-                nums[fast] = val;
+        int write = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[write] = nums[i];
+                write += 1;
             }
-            slow += 1;
-            fast += 1;
         }
-        if(nums[slow] == val) {
-            return slow;
-        }
-        return slow + 1;
+        return write;
     }
 }
